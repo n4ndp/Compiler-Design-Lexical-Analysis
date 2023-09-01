43b66f972b9c4fb02d280f0d788086090358ea1b
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-08-31 22:33:21 -0500

    primer avance de scanner

--- main.cpp
+++ main.cpp
@@ -93,10 +93,53 @@ public:
         // Aquí es donde implementarás la lógica para reconocer tokens
         // basados en el autómata finito y los estados definidos.
         // Retorna el token apropiado en cada caso.
+        Token* token;
+        char c = nextChar();
+
+        startLexeme();
+
+        while (1) {
+            switch (state) {
+                case 0: {
+                    // Estado inicial del autómata. Aquí se decide qué tipo de token reconocer.
+
+                    if (c == ' ' || c == '\t') // Si el carácter es un espacio en blanco o una tabulación, avanzamos al siguiente carácter.
+                        c = nextChar();
+                    else if (isalpha(c)) // Si el carácter es una letra, pasamos al estado 1 para reconocer palabras reservadas y IDs.
+                        state = 1;
+                    else if (isdigit(c)) // Si el carácter es un dígito, pasamos al estado 2 para reconocer números.
+                        state = 2;
+                    else if (c == ':') // Si el carácter es ':' podemos estar en proceso de reconocer una etiqueta.
+                        state = 3;
+                    else if (c == '\n') // Si el carácter es un salto de línea, pasamos al estado 4 para reconocer fin de línea.
+                        state = 4;
+                    else if (c == '\0') // Si el carácter es el final del archivo, pasamos al estado 5 para reconocer fin de archivo.
+                        state = 5;
+                    break;
+                }
+                case 1: {
+                    // Implementar lógica para reconocer palabras reservadas y IDs
+                    break;
+                }
+                case 2: {
+                    // Implementar lógica para reconocer números
+                    break;
+                }
+                case 3: {
+                    // Implementar lógica para reconocer etiquetas
+                    break;
+                }
+                case 4: {
+                    // Implementar lógica para reconocer fin de línea
+                    break;
+                }
+                case 5: {
+                    // Implementar lógica para reconocer fin de archivo
+                    break;
+                }
+            }
+        }
+
     }
     ~Scanner() = default;
 };
-
-
-
-
