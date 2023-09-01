106d67e6e26f8ded5a7095adacd09dc91128703f
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-09-01 08:07:45 -0500

    implementacion sin goto

--- scanner.cpp
+++ scanner.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
@@ -23,7 +22,7 @@ enum Type {
     SWAP,
     ADD,
     SUB,
-    MULT,
+    MUL,
     DIV, 
     STORE,
     LOAD,
@@ -50,7 +49,7 @@ const std::vector<std::string> tokenNames = {
     "SWAP",
     "ADD",
     "SUB",
-    "MULT",
+    "MUL",
     "DIV", 
     "STORE",
     "LOAD",
@@ -74,7 +73,7 @@ const std::map<std::string, Type> keywordMap = {
     {"swap", SWAP},
     {"add", ADD},
     {"sub", SUB},
-    {"mult", MULT},
+    {"mul", MUL},
     {"div", DIV},
     {"store", STORE},
     {"load", LOAD}
@@ -128,13 +127,15 @@ private:
     // Obtener el siguiente carácter del input
     char nextChar() {
         char c = input[current];
+        if (c != '\0')
             current++;
         return c;
     }
 
     // Retroceder la posición actual en el input
     void rollBack() {
-        current--;
+        if (input[current] != '\0')
+            current--;
         return;
     }
 
@@ -160,48 +161,33 @@ public:
         Token* token;
         char c = nextChar();
 
+        startLexeme();
+
         while (1) {
             switch (state) {
-                case 0: {
-                    // Estado inicial del autómata. Aquí se decide qué tipo de token reconocer.
-
-                    if (c == ' ' || c == '\t') {
-                        // Si el carácter es un espacio en blanco o una tabulación, avanzamos al siguiente carácter.
+                case 0: { // Estado inicial del autómata. Aquí se decide qué tipo de token reconocer.
+                    if (c == ' ' || c == '\t') // Si el carácter es un espacio en blanco o una tabulación, avanzamos al siguiente carácter.
                         c = nextChar();
-                    } else if (isalpha(c)) {
-                        // Si el carácter es una letra, pasamos al estado 1 para reconocer palabras reservadas y IDs.
+                    else if (isalpha(c)) // Si el carácter es una letra, pasamos al estado 1 para reconocer palabras reservadas y IDs.
                         state = 1;
-                    } else if (isdigit(c)) {
-                        // Si el carácter es un dígito, pasamos al estado 2 para reconocer números.
+                    else if (isdigit(c)) // Si el carácter es un dígito, pasamos al estado 2 para reconocer números.
                         state = 2;
-                    } else if (c == ':') {
-                        // Si el carácter es ':', cambiamos al estado 3 para reconocer etiquetas.
+                    else if (c == ':') // Si el carácter es ':' podemos estar en proceso de reconocer una etiqueta.
                         state = 3;
-                    } else if (c == '\n') {
-                        // Si el carácter es un salto de línea, pasamos al estado 4 para reconocer fin de línea.
+                    else if (c == '\n') // Si el carácter es un salto de línea, pasamos al estado 4 para reconocer fin de línea.
                         state = 4;
-                    } else if (c == '\0') {
-                        // Si el carácter es el final del archivo, pasamos al estado 5 para reconocer fin de archivo.
+                    else if (c == '\0') // Si el carácter es el final del archivo, pasamos al estado 5 para reconocer fin de archivo.
                         state = 5;
-                    } else {
-                        // Si el carácter no coincide con ninguno de los casos anteriores, es un error.
-                        token = new Token(ERR, std::string(1, c)); // Crear un token de error.
-                        state = 0; // Regresamos al estado inicial para reconocer el próximo token.
-                    }
-
                     break;
                 }
-                case 1: {
-                    // Estado para reconocer palabras reservadas y IDs.
-                    startLexeme();
+                case 1: { // Estado para reconocer palabras reservadas y IDs.
 
-                    while (isalnum(c) || c == '_') {
-                        // Mientras el carácter actual sea alfanumérico o un '_', avanzamos en el lexema.
+                    while (isalnum(c) || c == '_') { // Mientras el carácter actual sea alfanumérico o un '_', avanzamos en el lexema.
                         c = nextChar();
                     }
 
                     if (c == ':') {
-                        // Si el próximo carácter es ':', cambiamos al estado 3 para reconocer etiquetas.
+                        // Si el próximo carácter es ':', cambiamos al estado 4 para reconocer etiquetas.
                         state = 3;
                     } else {
                         // Si el próximo carácter no es ':', puede ser una palabra reservada válida o un ID.
@@ -213,62 +199,70 @@ public:
                         if (keywordToken) {
                             // Si es una palabra reservada, usamos el token de palabra reservada.
                             token = keywordToken;
+
+                            state = 0; // Regresamos al estado inicial para reconocer el próximo token.
                         } else {
-                            // Si no es una palabra reservada, asumimos que es un ID.
-                            token = new Token(ID, lexeme);
+                            // Si no es una palabra reservada, verificamos si es una combinación de "GOTO" y etiqueta.
+
+                            if (lexeme == "goto" && c == ' ') {
+                                // Si es una combinación de "goto" y etiqueta, cambiamos al estado 2 para manejar el GOTO.
+                                state = 2;
+
+                                c = nextChar(); // Esto es solo para revertir el rollBack, ahora c = ' ' (no cambia) y y current apunta a la misma posición de c.
+                            } else {
+                                // Si no es una combinación de "GOTO" y etiqueta, asumimos que es un ID.
+                                token = new Token(ID, lexeme);
+
+                                state = 0; // Regresamos al estado inicial para reconocer el próximo token.
+                            }
                         }
-                        state = 0; // Regresamos al estado inicial para reconocer el próximo token.
-                        return token;
                     }
 
                     break;
                 }
-                case 2: {
-                    // Estado para reconocer números.
-                    startLexeme();
 
-                    while (isdigit(c)) {
-                        // Mientras el carácter actual sea un dígito, avanzamos en el lexema.
+                case 2: { // Estado GOTO.
+                    // Usamos el token GOTO y cambiamos al estado 4 para reconocer etiquetas.
+                    token = new Token(GOTO);
+                    state = 4;
+
+                    c = nextChar(); // Esto es para que c sea el primer caracter del label y current apunte a el.
+
+                    break;
+                }
+                case 3: { // Estado para reconocer números.
+                    while (isdigit(c)) // Mientras el carácter actual sea un dígito, avanzamos en el lexema.
                         c = nextChar();
-                    }
-
+                    
                     rollBack(); // Retrocedemos para no incluir el último carácter no numérico.
+                    
                     std::string lexeme = getLexeme(); // Obtenemos el lexema acumulado.
                     token = new Token(NUM, lexeme); // Creamos un token de tipo NUM con el lexema.
 
-                    state = 0; // Regresamos al estado inicial para reconocer el próximo token.
-
-                    return token;
+                    state = 0; // Regresamos al estado inicial.
 
                     break;
                 }
-                case 3: {
-                    // Estado para reconocer etiquetas.
-
-                    rollBack(); // Retrocedemos para no incluir el último carácter no alfanumérico (acumulado en case 1).
-
+                case 4: { // Estado para reconocer etiquetas.
+                    while (isalnum(c) || c == '_') { // Mientras el carácter actual sea alfanumérico o un '_', avanzamos en el lexema.
+                        c = nextChar();
+                    }
+                    
+                    rollBack(); // Retrocedemos para no incluir el último carácter no alfanumérico.
+                    
                     std::string lexeme = getLexeme(); // Obtenemos el lexema acumulado.
                     token = new Token(LABEL, lexeme); // Creamos un token de tipo LABEL con el lexema.
+
                     state = 0; // Regresamos al estado inicial.
-
-                    c = nextChar();
-
-                    return token;
+                    break;
                 }
-                case 4: {
-                    // Estado para reconocer fin de línea.
-                    startLexeme();
-
-                    token = new Token(EOL); // Creamos un token de tipo EOL.
-                    state = 0; // Regresamos al estado inicial.
-                    return token;
-                }
-                case 5: {
-                    // Estado para reconocer fin de archivo.
-                    startLexeme();
+                case 5: { // Estado para reconocer fin de línea
                     
-                    token = new Token(END); // Creamos un token de tipo END.
-                    return token; // Salimos del bucle ya que no hay más tokens.
+                    break;
+                }
+                case 6: { // Estado para reconocer fin de archivo
+                    
+                    break;
                 }
             }
         }
@@ -276,20 +270,3 @@ public:
     }
     ~Scanner() = default;
 };
-
-int main(int argc, char** argv) {
-    std::fstream file(argv[1], std::ios::in);
-    std::stringstream stream;
-    stream << file.rdbuf();
-
-    Scanner scanner(stream.str().c_str());
-
-    Token* tk = scanner.nextToken();
-    while (tk->type != END) {
-        std::cout << "next token " << *tk << std::endl;
-        delete tk;
-        tk =  scanner.nextToken();
-    }
-
-    return 0;
-}
