85cdd1766089cdfa321c0258725be791e2a2e519
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-09-04 07:42:07 +0000

    no me token uwu

--- token.h
+++ token.h
@@ -6,7 +6,7 @@
 #include <vector>
 #include <unordered_map>
 
-// Enumeración que define los tipos de tokens posibles.
+// Enumeración que define los tokens posibles.
 enum Type {
     ID,
     NUM,
@@ -37,7 +37,7 @@ enum Type {
     END
 };
 
-// Vector que almacena los nombres de los tokens en el mismo orden que la enumeración Type.
+// Vector que almacena los nombres de los tokens.
 const std::vector<std::string> tokenNames = {
     "ID",
     "NUM",
@@ -68,7 +68,7 @@ const std::vector<std::string> tokenNames = {
     "END"
 };
 
-// Mapa que asocia palabras reservadas con sus tipos de token correspondientes.
+// Mapa que asocia palabras reservadas con sus tokens correspondientes.
 const std::unordered_map<std::string, Type> keywordMap = {
     {"push", PUSH},
     {"jmpeq", JMPEQ},
@@ -76,7 +76,7 @@ const std::unordered_map<std::string, Type> keywordMap = {
     {"jmpge", JMPGE},
     {"jmplt", JMPLT},
     {"jmple", JMPLE},
-    //{"goto", GOTO},
+    {"goto", GOTO},
     {"skip", SKIP},
     {"pop", POP},
     {"dup", DUP},
@@ -94,34 +94,33 @@ struct Token {
     Type type;
     std::string lexeme;
 
-    Token(Type t); // Constructor para tokens sin lexema.
-    Token(Type t, char c); // Constructor para tokens con lexema numérico o caracter.
-    Token(Type t, const std::string& s); // Constructor para tokens con lexema de cadena.
+    // Constructores.
+    Token(Type t);
+    Token(Type t, char c);
+    Token(Type t, const std::string& s);
 };
 
-Token::Token(Type t): type(t), lexeme("") { }; // Constructor para tokens sin lexema.
-
-Token::Token(Type t, char c): type(t), lexeme(std::to_string(c)) { }; // Constructor para tokens con lexema numérico o caracter.
-
-Token::Token(Type t, const std::string& s): type(t), lexeme(s) { }; // Constructor para tokens con lexema de cadena.
+// Constructores.
+Token::Token(Type t): type(t), lexeme("") { };
+Token::Token(Type t, char c): type(t), lexeme(std::to_string(c)) { };
+Token::Token(Type t, const std::string& s): type(t), lexeme(s) { };
 
 // Sobrecarga del operador de salida para imprimir información de un Token.
 std::ostream& operator<<(std::ostream& os, const Token& t) {
-    os << tokenNames[t.type]; // Imprime el nombre del tipo de token
+    os << tokenNames[t.type]; // Imprime el nombre del token.
     
-    // Si el token tiene un lexema, lo imprime entre paréntesis
-    if (!t.lexeme.empty())
-        os << "(" << t.lexeme << ")";
+    // Si el token tiene un lexema, lo imprime entre paréntesis.
+    if (!t.lexeme.empty()) os << "(" << t.lexeme << ")";
     
     return os;
 }
 
-// Función que verifica si un lexema es una palabra reservada (keyword).
+// Función que verifica si un token es una palabra reservada (keyword).
 Token* isKW(const std::string& l) {
     auto it = keywordMap.find(l);
-    if (it != keywordMap.end()) return new Token(it->second); // Devuelve un nuevo token con el tipo de token correspondiente a la palabra reservada.
+    if (it != keywordMap.end()) return new Token(it->second); // Devuelve un token correspondiente a la palabra reservada.
 
-    return nullptr; // Si el lexema no es una palabra reservada, devuelve nullptr (nulo).
+    return nullptr; // Si no es una palabra reservada, devuelve nullptr.
 }
 
 #endif // TOKEN_H
